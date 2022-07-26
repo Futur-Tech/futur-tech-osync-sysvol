c3b45f2d15803bd1ccfd637a8f2b04992fae3cb1
Guillaume Hullin <guillaume.hullin@futur-tech.com>
2022-07-26 20:10:42 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -123,7 +123,7 @@ if [ "$is_dc_master" = true ] ; then
 
     function custom_conf () {
         if grep "^${1}=" ${etc_file} ; then 
-            sed -i "s|^${1}=.*$|${1}=${2} # ${app_name}|" ${etc_file}123
+            sed -i "s|^${1}=.*$|${1}=${2} # ${app_name}|" ${etc_file}123 >/dev/null
             $S_LOG -s ${?/0/debug} -d $S_NAME -d "custom_conf" "${1}=${2} returned EXIT_CODE=$?"
         else
            $S_LOG -s crit -d $S_NAME -d "custom_conf" "Couldn't find ${1} in ${etc_file}"
